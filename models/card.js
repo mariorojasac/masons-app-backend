fcd5dbcf592a9895f399028364491adc706ca89d
mariorojasac <77466868+mariorojasac@users.noreply.github.com>
2021-10-28 16:35:32 -0400

    added schema values

--- models/card.js
+++ models/card.js
@@ -2,7 +2,11 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const cardSchema = new Schema({
-
+    title: String,
+    picture: String,
+    description: String,
+    sound: String, 
+    authorId: String,
 }, { timestamps: true })
 
 

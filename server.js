902cda44d2150b6898d0425597fedb970466c5cd
mariorojasac <77466868+mariorojasac@users.noreply.github.com>
2021-10-28 15:45:07 -0400

    wip added folders

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
 const mongoose = require("mongoose");
-const contactsController = require("./controllers/contacts");
+const controller = require("./controllers");
 
 const app = express();
 

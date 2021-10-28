39819bccd6ae6650a65eb9f7eb846108783393f2
mariorojasac <77466868+mariorojasac@users.noreply.github.com>
2021-10-28 15:43:07 -0400

    wip added middle ware

--- server.js
+++ server.js
@@ -12,6 +12,10 @@ const { DATABASE_URL, PORT = 3000 } = process.env;
 mongoose.connect(DATABASE_URL);
 const db = mongoose.connection;
 
+app.use(cors());
+app.use(express.json());
+app.use(morgan("dev"));
+
 db.on("connected", () => console.log("Connectes to MongoDB"));
 db.on("disconnected", () => console.log("Disconnected to MongoDb"));
 db.on("error", (error) => console.log("MongoDB has an error" + error.message));

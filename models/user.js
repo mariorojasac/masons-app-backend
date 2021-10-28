ac848012c29784c7443236c676840bbff41dac4e
mariorojasac <77466868+mariorojasac@users.noreply.github.com>
2021-10-28 19:37:12 -0400

    wip added user schema

--- models/user.js
+++ models/user.js
@@ -1,6 +1,14 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const userSchema = new Schema({}, { timestamps: true });
+const userSchema = new Schema({
+    name: String,
+    userName: String,
+    email: String,
+    profilePic: String,
+    admin: Boolean,
+    userId: String,
+    // cards: Objid,
+}, { timestamps: true });
 
 module.exports = mongoose.model("User", userSchema);

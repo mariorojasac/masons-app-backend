94901c56e75c8eff6e99b74ec23903a565a4a6b1
mariorojasac <77466868+mariorojasac@users.noreply.github.com>
2021-10-28 15:41:26 -0400

    wip added listening port

--- server.js
+++ server.js
@@ -3,3 +3,5 @@ const morgan = require("morgan");
 const cors = require("cors");
 const mongoose = require("mongoose");
 const contactsController = require("./controllers/contacts");
+
+app.listen(PORT, () => console.log(`Express is listening on port:${PORT}`));

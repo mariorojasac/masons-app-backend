9d364b6623909cfe8d88a21a703bd84c7eac0bb4
mariorojasac <77466868+mariorojasac@users.noreply.github.com>
2021-10-28 15:40:15 -0400

    added dependacies

---
+++ server.js
@@ -0,0 +1,5 @@
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const contactsController = require("./controllers/contacts");

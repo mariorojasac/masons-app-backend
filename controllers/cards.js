1dabd9ca943d0c12348387ce1fb27d3fcfec3e19
mariorojasac <77466868+mariorojasac@users.noreply.github.com>
2021-10-28 20:35:06 -0400

    wip adding routes

--- controllers/cards.js
+++ controllers/cards.js
@@ -2,4 +2,22 @@ const express = require('express')
 const Card = require('../models/card')
 const router = express.Router()
 
+// routes
+
+router.get('/', async (req, res) => {
+    try {
+        res.json(await Card.find({}))
+    } catch (error) {
+        res.status(401).json({message: 'Please login to see cards'})
+    }
+})
+
+router.post('/', async (req, res) => {
+    try {
+        res.json(await Card.create(req, body))
+    } catch (error) {
+        res.status(401).json({message: 'Please login to create card'})
+    }
+})
+
 module.exports = router
--- server.js
+++ server.js
@@ -12,12 +12,21 @@ const { DATABASE_URL, PORT = 3000 } = process.env;
 mongoose.connect(DATABASE_URL);
 const db = mongoose.connection;
 
+db.on("connected", () => console.log("Connectes to MongoDB"));
+db.on("disconnected", () => console.log("Disconnected to MongoDb"));
+db.on("error", (error) => console.log("MongoDB has an error" + error.message));
+
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
 
-db.on("connected", () => console.log("Connectes to MongoDB"));
-db.on("disconnected", () => console.log("Disconnected to MongoDb"));
-db.on("error", (error) => console.log("MongoDB has an error" + error.message));
+
+// routes
+app.get()
+
+app.use()
+
+app.get()
+
 
 app.listen(PORT, () => console.log(`Express is listening on port:${PORT}`));

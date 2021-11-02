a734c30f87ac631a402712e2f77309c2264ced60
mariorojasac <77466868+mariorojasac@users.noreply.github.com>
2021-11-02 13:43:02 -0400

    modified card schema

--- models/user.js
+++ models/user.js
@@ -10,9 +10,23 @@ const userSchema = new Schema(
     profilePic: { type: String, required: true },
     admin: { type: Boolean, default: false },
     userId: { type: String, unique: true, required: true },
-    // cards: Objid,
+    cards : [cardSchema]
   },
   { timestamps: true }
 );
 
+const cardSchema = new Schema(
+  {
+    title: String,
+    picture: String,
+    description: String,
+    sound: String,
+    type: String,
+    authorId: String,
+  },
+  { timestamps: true }
+);         
+
+
 module.exports = mongoose.model("User", userSchema);
+module.exports = mongoose.model("Card", cardSchema)

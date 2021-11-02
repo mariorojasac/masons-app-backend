06101e755f3f92b8ff21fa6f3c10692cc81e4cc2
mariorojasac <77466868+mariorojasac@users.noreply.github.com>
2021-11-02 13:37:06 -0400

    adding to schema

--- models/user.js
+++ models/user.js
@@ -1,14 +1,18 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const userSchema = new Schema({
-    name: String,
-    userName: String,
-    email: String,
-    profilePic: String,
-    admin: Boolean,
-    userId: String,
+const userSchema = new Schema(
+  {
+    name: { type: String, unique: true, required: true },
+    username: { type: String, unique: true, required: true },
+    password: { type: String, required: true },
+    email: { type: String, unique: true, required: true },
+    profilePic: { type: String, required: true },
+    admin: { type: Boolean, default: false },
+    userId: { type: String, unique: true, required: true },
     // cards: Objid,
-}, { timestamps: true });
+  },
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("User", userSchema);
